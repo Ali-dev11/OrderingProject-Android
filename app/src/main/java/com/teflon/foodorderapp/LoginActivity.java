23d81e36cbc2c113d76c60f87162f38e081b0583
teflon-code <hello@oucit.edu.pk>
2021-05-31 00:20:31 +0500

    Intents set and Launcher Activity

--- app/src/main/java/com/teflon/foodorderapp/LoginActivity.java
+++ app/src/main/java/com/teflon/foodorderapp/LoginActivity.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     EditText username,password;
-    Button btnLogin;
+    Button btnLogin,btnsignup;
     DBHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
         username=(EditText)findViewById(R.id.username1);
         password=(EditText)findViewById(R.id.password1);
         btnLogin=(Button)findViewById(R.id.btnsignin1);
+        btnsignup=(Button)findViewById(R.id.btnsignup1);
         db=new DBHelper(this);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -28,12 +29,11 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String user= username.getText().toString();
                 String pass=password.getText().toString();
-
                 if (user.equals("")||pass.equals("")){
                     Toast.makeText(LoginActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
                 }else{
                     Boolean checkuserpass=db.checkusernamepassword(user,pass);
-                    if(checkuserpass==true){
+                    if(checkuserpass == true){
                         Toast.makeText(LoginActivity.this, "Sign in Successfully", Toast.LENGTH_SHORT).show();
                         Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                         startActivity(intent);
@@ -41,7 +41,13 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(LoginActivity.this, "Username and Password is Incorrect", Toast.LENGTH_SHORT).show();
                     }
                 }
-
+            }
+        });
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
 
             }
         });
