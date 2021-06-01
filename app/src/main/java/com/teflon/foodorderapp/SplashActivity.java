34f126d350eea342b7383f3a02ec41ebff0cfe3f
teflon-code <hello@oucit.edu.pk>
2021-06-01 22:16:22 +0500

    animation added

--- app/src/main/java/com/teflon/foodorderapp/SplashActivity.java
+++ app/src/main/java/com/teflon/foodorderapp/SplashActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
-
+TextView textView;
+Animation fadein;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+textView=findViewById(R.id.spText);
+
+fadein= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+
+textView.setVisibility(View.VISIBLE);
+textView.startAnimation(fadein);
+
+
+
+
 
         Thread td=new Thread(){
             public void run(){
